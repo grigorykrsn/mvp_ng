201b46c2fb51ef7e3bd26f0155032d180bf01b8a
grigorykrsn <grigory.krsn@gmail.com>
2022-04-26 17:42:48 +0300

    add forecast-info module and filter selects

--- src/app/components/forecast-block/forecast-block.module.ts
+++ src/app/components/forecast-block/forecast-block.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ForecastChartModule } from '../forecast-chart/forecast-chart.module';
+import { ForecastInfoModule } from '../forecast-info/forecast-info.module';
 
 import { ForecastBlockComponent } from './forecast-block.component';
 
 @NgModule({
   declarations: [ForecastBlockComponent],
   exports: [ForecastBlockComponent],
-  imports: [CommonModule, ForecastChartModule],
+  imports: [CommonModule, ForecastChartModule, ForecastInfoModule],
 })
 export class ForecastBlockModule {}
