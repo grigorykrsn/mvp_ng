f77d465c2418ddbe6a1a5ab46c0e443cfe095507
IvanCherkas <4erkas.ivan@gmail.com>
2022-04-26 13:17:36 +0300

    add forcast block

--- src/app/pages/main-page/main-page.module.ts
+++ src/app/pages/main-page/main-page.module.ts
@@ -4,17 +4,21 @@ import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 
+import { ForecastBlockModule } from '../../components/forecast-block/forecast-block.module';
+
 import { MainPageComponent } from './main-page.component';
 import { MainPageRoutingModule } from './main-page-routing.module';
+
 
 @NgModule({
   declarations: [MainPageComponent],
   exports: [MainPageComponent],
   imports: [
-    CommonModule, 
+    CommonModule,
     MatSidenavModule,
     MatButtonModule,
-    MainPageRoutingModule
+    MainPageRoutingModule,
+    ForecastBlockModule,
   ],
 })
 export class MainPageModule {}
